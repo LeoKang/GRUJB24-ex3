305fe8ab46e8f6aab6bc7ddc94a9d22078371b56
LeoKang <xeungjin@nate.com>
2024-07-15 14:41:25 +0900

    ~ 117 page

--- src/main/java/org/zerock/ex3/controller/SampleController.java
+++ src/main/java/org/zerock/ex3/controller/SampleController.java
@@ -1,8 +1,8 @@
 package org.zerock.ex3.controller;
 
 import lombok.extern.log4j.Log4j2;
-import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -17,9 +17,12 @@ import java.util.stream.IntStream;
 @RequestMapping("/sample")
 @Log4j2
 public class SampleController {
+
     @GetMapping("/ex1")
-    public void ex1() {
-        log.info("ex1.........");
+    public void ex1(){
+
+        log.info("ex1...............");
+
     }
 
     @GetMapping({"/ex2", "/exLink"})
@@ -38,14 +41,16 @@ public class SampleController {
         model.addAttribute("list", list);
     }
 
+
     @GetMapping({"/exInline"})
-    public String exInline(RedirectAttributes redirectAttributes) {
-        log.info("exInline................");
+    public String exInline(RedirectAttributes redirectAttributes){
+
+        log.info("exInline..............");
 
         SampleDTO dto = SampleDTO.builder()
                 .sno(100L)
-                .first("First...100")
-                .last("Last...100")
+                .first("First..100")
+                .last("Last..100")
                 .regTime(LocalDateTime.now())
                 .build();
         redirectAttributes.addFlashAttribute("result", "success");
@@ -59,7 +64,7 @@ public class SampleController {
         log.info("ex3");
     }
 
-    @GetMapping("/exLayout1")
+    @GetMapping({"/exLayout1", "/exLayout2", "/exTemplate"})
     public void exLayout1() {
         log.info("exLayout.......");
     }
